01e4ddbb35b952acbd0d5539d602700291fa830a
zerotonyq <giga.a.n.t@mail.ru>
2023-06-26 20:14:45 +0300

    KP8

---
+++ Gigolaev/KP8/src/Node.cpp
@@ -0,0 +1,6 @@
+#include "Node.hpp"
+
+Node::Node(char v)
+{
+	value = v;
+}
---
+++ Gigolaev/KP8/tests/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include "List.hpp"
+int main()
+{
+    Node* a = new Node('a');
+    Node* b = new Node('b');
+    List* l = new List(a, new Node('c'));
+
+    l->print();
+    std::cout << l->length() << std::endl;
+    l->insert(a, b);
+    l->insert(b, new Node('d'));
+    l->insert(b, new Node('e'));
+    l->print();
+    std::cout << l->length() << std::endl;
+    //l->erase(a);
+    l->print();
+    std::cout << l->length() << std::endl;
+    l->change_places();
+    l->print();
+
+}
