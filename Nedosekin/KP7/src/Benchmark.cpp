b7636a19b2416d33103f2e5a952530ee5d63e020
Dots109 <113204425+Dots109@users.noreply.github.com>
2023-06-27 14:04:24 +0300

    Kp7change benchmark (#156)
    Co-authored-by: Maxsmile123 <sysoevmaksim27@gmail.com>

--- Nedosekin/KP7/src/Benchmark.cpp
+++ Nedosekin/KP7/src/Benchmark.cpp
@@ -4,60 +4,76 @@
 
 void benchmark()
 {
-    std::cout << "Initialization:" << std::endl;
+    
 
-    std::cout << "My vector:" << std::endl;
+    std::cout << "Push_back(50)" << std::endl;
+    std::cout << "Library vector:" << std::endl;
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    Vector<int> Vec1(1000000);
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-
-    std::cout << "Library vector:" << std::endl;
-    begin = std::chrono::steady_clock::now();
-    std::vector<int> vec1(1000000);
-    end = std::chrono::steady_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-
-    std::cout << "Push_back" << std::endl;
-    std::cout << "Library vector:" << std::endl;
-    begin = std::chrono::steady_clock::now();
     Vector<int> Vec2;
-    for (int64_t pushValue = 0; pushValue < 1000000; ++pushValue) {
+    for (int64_t pushValue = 0; pushValue < 50; ++pushValue) {
         Vec2.push_back(pushValue);
     }
-    end = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
     std::cout << "My vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
     std::vector<int> vec2;
-    for (int64_t pushValue = 0; pushValue < 1000000; ++pushValue) {
+    for (int64_t pushValue = 0; pushValue < 50; ++pushValue) {
         vec2.push_back(pushValue);
     }
     end = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     std::cout << "-----------------------------" << std::endl;
-    std::cout << "By-pass:" << std::endl;
+
+
+
+
+    std::cout << "push_back(1000):" << std::endl;
+
     std::cout << "My vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    for (int64_t passCount = 0; passCount < 1000000; ++passCount) {
-        int a = Vec2[passCount];
+    for (int64_t popCount = 0; popCount < 1000; ++popCount) {
+        Vec2.pop_back();
     }
     end = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-
     std::cout << "Library vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    for (int64_t passCount = 0; passCount < 1000000; ++passCount) {
-        int a = vec2[passCount];
+    for (int64_t popCount = 0; popCount < 1000; ++popCount) {
+        vec2.pop_back();
     }
     end = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     std::cout << "-----------------------------" << std::endl;
-    std::cout << "Pop_back:" << std::endl;
+
+
+
+
+    std::cout << "push_back(10000):" << std::endl;
+
+    std::cout << "My vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 10000; ++popCount) {
+        Vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    std::cout << "Library vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 10000; ++popCount) {
+        vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+
+
+
+
+    std::cout << "push_back(10^6):" << std::endl;
 
     std::cout << "My vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
@@ -75,96 +91,140 @@ void benchmark()
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     std::cout << "-----------------------------" << std::endl;
-    std::cout << "Clear:" << std::endl;
 
-    std::cout << "Library vector:" << std::endl;
-    begin = std::chrono::steady_clock::now();
-    vec2.clear();
-    end = std::chrono::steady_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+
+
+    std::cout << "push_back(10^7):" << std::endl;
 
     std::cout << "My vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    Vec2.clear();
-    end = std::chrono::steady_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << "Reserve(10000):" << std::endl;
-
-    std::cout << "My vector:" << std::endl;
-    begin = std::chrono::steady_clock::now();
-    for (size_t reserveSize = 1; reserveSize < 10000; ++reserveSize) {
-        Vec2.reserve(reserveSize);
-    }
-    end = std::chrono::steady_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-
-    std::cout << "Library vector:" << std::endl;
-    begin = std::chrono::steady_clock::now();
-    for (size_t reserveSize = 1; reserveSize < 10000; ++reserveSize) {
-        vec2.reserve(reserveSize);
-    }
-    end = std::chrono::steady_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
-
-    std::cout << "-----------------------------" << std::endl;
-    std::cout << "Reserve(100000):" << std::endl;
-
-    std::cout << "My vector:" << std::endl;
-    begin = std::chrono::steady_clock::now();
-    for (size_t reserveSize = 1; reserveSize < 100000; ++reserveSize) {
-        Vec2.reserve(reserveSize);
+    for (int64_t popCount = 0; popCount < 10000000; ++popCount) {
+        Vec2.pop_back();
     }
     end = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
     std::cout << "Library vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    for (size_t reserveSize = 1; reserveSize < 100000; ++reserveSize) {
-        vec2.reserve(reserveSize);
+    for (int64_t popCount = 0; popCount < 10000000; ++popCount) {
+        vec2.pop_back();
     }
     end = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     std::cout << "-----------------------------" << std::endl;
-    std::cout << "Equalses:" << std::endl;
-    Vector<int> Vec3(1000000);
-    Vector<int> Vec4(1000000);
-    std::vector<int> vec3(1000000);
-    std::vector<int> vec4(1000000);
 
-    std::cout << "operator==:" << std::endl;
+ 
+    std::cout << "Pop_back(50):" << std::endl;
 
     std::cout << "My vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    if (Vec3 == Vec4) {
-        end = std::chrono::steady_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    for (int64_t popCount = 0; popCount < 50; ++popCount) {
+        Vec2.pop_back();
     }
-
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
     std::cout << "Library vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    if (vec3 == vec4) {
-        end = std::chrono::steady_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    for (int64_t popCount = 0; popCount < 50; ++popCount) {
+        vec2.pop_back();
     }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     std::cout << "-----------------------------" << std::endl;
-    std::cout << "operator!=:" << std::endl;
+
+
+
+
+    std::cout << "Pop_back(1000):" << std::endl;
 
     std::cout << "My vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    if (!(Vec3 != Vec4)) {
-        end = std::chrono::steady_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    for (int64_t popCount = 0; popCount < 1000; ++popCount) {
+        Vec2.pop_back();
     }
-
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
     std::cout << "Library vector:" << std::endl;
     begin = std::chrono::steady_clock::now();
-    if (!(vec3 != vec4)) {
-        end = std::chrono::steady_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    for (int64_t popCount = 0; popCount < 1000; ++popCount) {
+        vec2.pop_back();
     }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
 
     std::cout << "-----------------------------" << std::endl;
+
+
+
+
+
+    std::cout << "Pop_back(10000):" << std::endl;
+
+    std::cout << "My vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 10000; ++popCount) {
+        Vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    std::cout << "Library vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 10000; ++popCount) {
+        vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+
+
+
+
+
+    std::cout << "Pop_back(10^6):" << std::endl;
+
+    std::cout << "My vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 1000000; ++popCount) {
+        Vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    std::cout << "Library vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 1000000; ++popCount) {
+        vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+
+
+
+
+
+
+    std::cout << "Pop_back(10^7):" << std::endl;
+
+    std::cout << "My vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 10000000; ++popCount) {
+        Vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+    std::cout << "Library vector:" << std::endl;
+    begin = std::chrono::steady_clock::now();
+    for (int64_t popCount = 0; popCount < 10000000; ++popCount) {
+        vec2.pop_back();
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+ 
+
 }
